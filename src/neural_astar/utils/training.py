436a2babdf9dfa260c52125619d44da8a9b4f39b
Ryo Yonetani <yonetani.vision@gmail.com>
2022-12-19 11:34:10 +0900

    4 introduce ptl (#9)
    * Introduce PTL functions
    
    * Update checkpoint
    
    * Update dockerfile
    
    * fix scripts

--- src/neural_astar/utils/training.py
+++ src/neural_astar/utils/training.py
@@ -6,113 +6,88 @@ Affiliation: OSX
 from __future__ import annotations
 
 import random
-from dataclasses import dataclass
-from typing import Sequence, Tuple, Union
+import re
+from glob import glob
 
 import numpy as np
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
+import torch.optim
+from neural_astar.planner.astar import VanillaAstar
 from neural_astar.planner.differentiable_astar import AstarOutput
 from PIL import Image
-from torch.nn.modules.loss import _Loss
 from torchvision.utils import make_grid
 
-EPS = 1e-10
 
-
-@dataclass
-class Metrics:
-    p_opt: float
-    p_exp: float
-    h_mean: float
-
-    def __repr__(self):
-        return f"optimality: {self.p_opt:0.3f}, efficiency: {self.p_exp:0.3f}, h_mean: {self.h_mean:0.3f}"
-
-
-def run_planner(
-    batch: Tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor],
-    planner: nn.Module,
-    criterion: _Loss,
-) -> Tuple[torch.tensor, AstarOutput]:
+def load_from_ptl_checkpoint(checkpoint_path: str) -> dict:
     """
-    Run planner on a given batch
+    Load model weights from PyTorch Lightning checkpoint.
 
     Args:
-        batch (Tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor]): input batch
-        planner (nn.Module): planner
-        criterion (_Loss): loss function
+        checkpoint_path (str): (parent) directory where .ckpt is stored.
 
     Returns:
-        Tuple[torch.tensor, AstarOutput]: computed loss + planner output
+        dict: model state dict
     """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    map_designs, start_maps, goal_maps, opt_trajs = batch
-    map_designs = map_designs.to(device)
-    start_maps = start_maps.to(device)
-    goal_maps = goal_maps.to(device)
-    opt_trajs = opt_trajs.to(device)
-    planner_outputs = planner(map_designs, start_maps, goal_maps)
-    loss = criterion(planner_outputs.histories, opt_trajs)
 
-    return loss, planner_outputs
+    ckpt_file = sorted(glob(f"{checkpoint_path}/**/*.ckpt", recursive=True))[-1]
+    print(f"load {ckpt_file}")
+    state_dict = torch.load(ckpt_file)["state_dict"]
+    state_dict_extracted = dict()
+    for key in state_dict:
+        if "planner" in key:
+            state_dict_extracted[re.split("planner.", key)[-1]] = state_dict[key]
 
-
-def calc_metrics(na_outputs: AstarOutput, va_outputs: AstarOutput) -> Metrics:
-    """
-    Calculate opt, exp, and hmean metrics for problem instances each with a single starting point
-
-    Args:
-        na_outputs (AstarOutput): outputs from Neural A*
-        va_outputs (AstarOutput): outputs from vanilla A*
-
-    Returns:
-        Metrics: opt, exp, and hmean values
-    """
-    pathlen_astar = va_outputs.paths.sum((1, 2, 3)).detach().cpu().numpy()
-    pathlen_na = na_outputs.paths.sum((1, 2, 3)).detach().cpu().numpy()
-    p_opt = (pathlen_astar == pathlen_na).mean()
-
-    exp_astar = va_outputs.histories.sum((1, 2, 3)).detach().cpu().numpy()
-    exp_na = na_outputs.histories.sum((1, 2, 3)).detach().cpu().numpy()
-    p_exp = np.maximum((exp_astar - exp_na) / exp_astar, 0.0).mean()
-
-    h_mean = 2.0 / (1.0 / (p_opt + EPS) + 1.0 / (p_exp + EPS))
-
-    return Metrics(p_opt, p_exp, h_mean)
+    return state_dict_extracted
 
 
-def calc_metrics_from_multiple_results(
-    na_outputs_list: Sequence[AstarOutput], va_outputs_list: Sequence[AstarOutput]
-) -> Metrics:
-    """
-    Calculate opt, exp, and hmean metrics for problem instances each with multiple starting points
+class PlannerModule(pl.LightningModule):
+    def __init__(self, planner, config):
+        super().__init__()
+        self.planner = planner
+        self.vanilla_astar = VanillaAstar()
+        self.config = config
 
-    Args:
-        na_outputs (Sequence[AstarOutput]): Sequence of outputs from Neural A*
-        va_outputs (Sequence[AstarOutput]): Sequence of outputs from vanilla A*
+    def forward(self, map_designs, start_maps, goal_maps):
+        return self.planner(map_designs, start_maps, goal_maps)
 
-    Returns:
-        Metrics: opt, exp, and hmean values
-    """
-    p_opt_list, p_exp_list = [], []
-    for na_outputs, va_outputs in zip(na_outputs_list, va_outputs_list):
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        return torch.optim.RMSprop(self.planner.parameters(), self.config.params.lr)
+
+    def training_step(self, train_batch, batch_idx):
+        map_designs, start_maps, goal_maps, opt_trajs = train_batch
+        outputs = self.forward(map_designs, start_maps, goal_maps)
+        loss = nn.L1Loss()(outputs.histories, opt_trajs)
+        self.log("metrics/train_loss", loss)
+
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        map_designs, start_maps, goal_maps, opt_trajs = val_batch
+        outputs = self.forward(map_designs, start_maps, goal_maps)
+        loss = nn.L1Loss()(outputs.histories, opt_trajs)
+        va_outputs = self.vanilla_astar(map_designs, start_maps, goal_maps)
         pathlen_astar = va_outputs.paths.sum((1, 2, 3)).detach().cpu().numpy()
-        pathlen_na = na_outputs.paths.sum((1, 2, 3)).detach().cpu().numpy()
-        p_opt_list.append(pathlen_astar == pathlen_na)
+        pathlen_model = outputs.paths.sum((1, 2, 3)).detach().cpu().numpy()
+        p_opt = (pathlen_astar == pathlen_model).mean()
 
         exp_astar = va_outputs.histories.sum((1, 2, 3)).detach().cpu().numpy()
-        exp_na = na_outputs.histories.sum((1, 2, 3)).detach().cpu().numpy()
-        p_exp_list.append(np.maximum((exp_astar - exp_na) / exp_astar, 0.0))
-    p_opt = np.vstack(p_opt_list).mean(0)
-    p_exp = np.vstack(p_exp_list).mean(0)
-    h_mean = 2.0 / (1.0 / (p_opt + EPS) + 1.0 / (p_exp + EPS))
+        exp_na = outputs.histories.sum((1, 2, 3)).detach().cpu().numpy()
+        p_exp = np.maximum((exp_astar - exp_na) / exp_astar, 0.0).mean()
 
-    return Metrics(p_opt.mean(), p_exp.mean(), h_mean.mean())
+        h_mean = 2.0 / (1.0 / (p_opt + 1e-10) + 1.0 / (p_exp + 1e-10))
+
+        self.log("metrics/val_loss", loss)
+        self.log("metrics/p_opt", p_opt)
+        self.log("metrics/p_exp", p_exp)
+        self.log("metrics/h_mean", h_mean)
+
+        return loss
 
 
 def visualize_results(
-    map_designs: torch.tensor, planner_outputs: Union[AstarOutput, dict], scale: int = 1
+    map_designs: torch.tensor, planner_outputs: AstarOutput, scale: int = 1
 ) -> np.ndarray:
     """
     Create a visualization of search results
